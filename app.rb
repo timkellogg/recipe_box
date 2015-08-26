547df30fec238bf3a9b98a5377259b702d64b7a6
Alisa Colon & Tim Kellogg <student@epicodus.com>
2015-08-26 12:03:42 -0700

    add basic admin pages

--- app.rb
+++ app.rb
@@ -1,4 +1,48 @@
 require 'bundler/setup'
-Bundler.require :default
+Bundler.require :default, :test
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+  erb(:index)
+end
+
+get '/admin_recipe_box' do
+  @categories = Category.all
+  erb :recipe_create_form
+end
+
+get '/admin_portal' do
+  @categories = Category.all
+  @recipes = Recipe.all
+  @ingredients = Ingredient.all
+  erb :recipe_crud
+end
+
+post '/admin_portal' do
+  instructions = params['instructions']
+  dish_name    = params['dish_name']
+  pic_link     = params['pic_link']
+  rating       = params['rating'].to_i
+
+  ingredients = params['ingredients'].split(',')
+  ingredients.each do |ingredient|
+    if Ingredient.find_by(item: ingredient)
+      @recipe.ingredient.push(ingredient)
+    else
+      Ingredient.create(item: ingredient)
+      @recipe.ingredient.push(ingredient)
+    end
+  end
+
+
+  @category = Category.find(params['category'])
+  @category.update({recipe_id: @recipe.id, ingredient_id: ingredient.id })
+
+  @recipe = Recipe.create({ instructions: instructions, dish_name: dish_name, pic_link: pic_link, rating: rating })
+  binding.pry
+  categories = params
+
+
+  erb :recipe_crud
+end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -4,4 +4,6 @@ class Recipe < ActiveRecord::Base
 
   validates :dish_name, length: { in: 2..100 }
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+
+  
 end
