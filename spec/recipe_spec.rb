b8859db3c77eb28d0fe3bea43700b8f214ec4018
Alisa Colon & Tim Kellogg <student@epicodus.com>
2015-08-26 10:39:23 -0700

    add more validations to recipe

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -7,13 +7,24 @@ describe Recipe do
     expect(recipe.save).to eq false
   end
 
-  it 'returns false if integer is greater than 5 and less than 0' do
+  it 'returns false if integer is a number' do
     recipe = Recipe.new({instructions: 'put the lime in the coconut', rating: 'bob', dish_name: 'coco-lime', pic_link: 'www.coco.com'})
     expect(recipe.save).to eq false
   end
 
-  it 'returns false if integer is less than 0' do
+  it 'returns false if rating is less than 0' do
     recipe = Recipe.new({instructions: 'put the lime in the coconut', rating: -1, dish_name: 'coco-lime', pic_link: 'www.coco.com'})
     expect(recipe.save).to eq false
   end
+
+  it 'returns false if rating is greater than 5' do
+    recipe = Recipe.new({instructions: 'put the lime in the coconut', rating: 6, dish_name: 'coco-lime', pic_link: 'www.coco.com'})
+    expect(recipe.save).to eq false
+  end
+
+  it 'returns false if rating is not an integer' do
+    recipe = Recipe.new({instructions: 'put the lime in the coconut', rating: 6.092034, dish_name: 'coco-lime', pic_link: 'www.coco.com'})
+    expect(recipe.save).to eq false
+  end
+
 end
