387b6f3a451bc979cfcd3b04c3f657b1d5ad0dd3
alisa colon & Tim Kellogg <student@epicodus.com>
2015-08-27 16:46:05 -0700

    more styling improvments

--- lib/category.rb
+++ lib/category.rb
@@ -1,12 +1,4 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :recipes
-
-  # before_save down_case
-  # validates :dish_type, uniqueness: true
-  #
-  # private
-  #
-  #   def down_case
-  #     dish_type.downcase!
-  #   end
+  validates :dish_type, uniqueness: true
 end
